242f30c3ab89f11e434ede6b012b4160d4485d91
Lewis Sparlin <lewis.sparlin@icloud.com>
2017-10-09 19:11:51 -0500

    some stabilization maybe

--- src/js/components/sequence/SequenceActionsButtonGroup.jsx
+++ src/js/components/sequence/SequenceActionsButtonGroup.jsx
@@ -20,7 +20,7 @@ class SequenceActionsButtonGroup extends React.Component {
         <Button className="float-right" color="light" disabled={!this.props.enableButtons} onClick={this.props.startSequence}>Run Sequence</Button>
 
         <Dropdown isOpen={this.state.isOpen} toggle={this.toggle}>
-          <DropdownToggle caret color="light" />
+          <DropdownToggle caret color="light" onClick={this.toggle} data-toggle="dropdown" aria-expanded={this.state.isOpen} />
           <DropdownMenu right>
             <DropdownItem onClick={this.props.onDelete}><span className="fa fa-trash"></span> Delete</DropdownItem>
           </DropdownMenu>
